6d1af9f28aa1b8bec74431ab353b1e1477694210
E-VadnieLewis <evadnielewis@gmail.com>
2024-01-29 11:32:50 -0500

    my first commit

--- index.js
+++ index.js
@@ -1,2 +1,9 @@
 const exampleDinosaurData = require("./data/dinosaurs");
 /// Program your functions below //
+getLongestDinosaur();
+getDinosaurDescription();
+getDinosaursAliveMya();
+getRoomByDinosaurName();
+getConnectedRoomNamesByld();
+calculateTicketPrice();
+purchaseTickets();
